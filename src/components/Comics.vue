b95f04167046b095fe8522650b8dc0126cabb1b2
Salano24 <salernomarco48@email.com>
2022-11-11 16:23:10 +0100

    fine esercizio

---
+++ src/components/Comics.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+name: 'Comic',
+props:{
+    image: String,
+    title: String
+},
+}
+</script>
+
+<template>
+    <div class="col">
+        <div class="comic">
+            <img :src="thumb" alt="">
+            <h4>{{ series }}</h4>
+        </div>
+    </div>
+</template>
--- src/components/SiteJumbo.vue
+++ src/components/SiteJumbo.vue
@@ -1,19 +1,38 @@
 <script>
+import comics from '../dc-comics.js'
 export default{
-    name: 'SiteHeader',
+
+  props:{
+    image: String,
+    title: String
+},
+    name: 'SiteJumbo',
     data() {
       return {
-        
+        comics: comics
       }
     }
 }
 </script>
 
 <template>
-      <div class="jumbo">
-    <div class="container">
-      
-      <h3>--> Content goes here &lt--</h3>
+      <div class="jumbo"> 
+        <div class="current">
+         <img src="../assets/img/jumbotron.jpg" alt=""> 
+        </div>
+      <div class="container">
+        <div class="row">
+        <div class="col-2" v-for="comic in comics">
+        <div class="comic">
+            <img :src="comic.thumb" alt="">
+            <h4>{{ comic.series }}</h4>
+        </div>
+      </div>
+    </div> 
+    <div class="load">
+      <button>LOAD MORE</button>
     </div>
-  </div>
+      </div>
+    </div>
+ 
 </template>
